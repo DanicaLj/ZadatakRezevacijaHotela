0f0e2b33b8deddb680779e950ebbbaaaa3826ca8
DanicaLj <55446720+DanicaLj@users.noreply.github.com>
2020-06-26 13:51:42 +0200

    Update Program.cs
    deleted comments

--- solZadatak/Zadatak/Program.cs
+++ solZadatak/Zadatak/Program.cs
@@ -42,21 +42,7 @@ namespace Zadatak
 
             }
 
-            //writes matix of rooms and days 1000x366
-            /*int rowLength = mat.GetLength(0);
-            int colLength = mat.GetLength(1);
-
-            for (int i = 0; i < rowLength; i++)
-            {
-                for (int j = 0; j < colLength; j++)
-                {
-                    Console.Write(string.Format("{0} ", mat[i, j]));
-                }
-                Console.Write(Environment.NewLine + Environment.NewLine);
-            }*/
-
             Console.ReadLine();
-
         }
 
 
